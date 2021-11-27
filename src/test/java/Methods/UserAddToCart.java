e3037397b3533d2d615bc722b435c8022883a4ad
pawelgajger <pawelgajger@gmail.com>
2021-11-27 18:51:31 +0100

    "zadanie2"

--- src/test/java/Methods/UserAddToCart.java
+++ src/test/java/Methods/UserAddToCart.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class UserAddToCart {
+
     private WebDriver driver;
 
     @FindBy(xpath = "//img[@alt='PrestaShop']")
